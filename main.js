6ac9081512fddb1a6c7f5203df3a47dd80cd9696
VitaliyFedyay <fedyay.vitaliy@gmail.com>
2019-12-18 12:58:07 +0200

    some updates

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-var slicedToArray = function () {
+let slicedToArray = function () {
   function sliceIterator(arr, i) {
-    var arr = []; var n = true; var d = false; var e = undefined;
+    let n = true; let d = false; let e = undefined;
     try {
-      for (var i = arr[Symbol.iterator](), s; !(n = (s = i.next()).done); n = true) {
+      for (let i = arr[Symbol.iterator](), s; !(n = (s = i.next()).done); n = true) {
         arr.push(s.value);
         if (i && arr.length === i) break;
       }
@@ -28,9 +28,9 @@ var slicedToArray = function () {
 
 // Vector
 
-var Vector = function Vector() {
-  var x = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
-  var y = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
+let Vector = function Vector() {
+  let x = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
+  let y = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
 
   this.x = x;
   this.y = y;
@@ -57,7 +57,7 @@ Vector.prototype.multiply = function (v) {
 };
 
 Vector.prototype.additiveInverseOf = function (v) {
-  var sum = this.add(v);
+  let sum = this.add(v);
   if (sum.x === 0 && sum.y === 0) {
     return true;
   }
@@ -65,13 +65,14 @@ Vector.prototype.additiveInverseOf = function (v) {
 };
 
 Vector.random = function (max, size) {
-  return new Vector(Math.floor(Math.random() * (max.x / size.x)) * size.x, Math.floor(Math.random() * (max.y / size.y)) * size.y);
+  return new Vector(Math.floor(Math.random() * (max.x / size.x)) * size.x,
+    Math.floor(Math.random() * (max.y / size.y)) * size.y);
 };
 
 // Drawing
 
-var canvas = document.getElementById('snake');
-var ctx = canvas.getContext('2d');
+let canvas = document.getElementById('snake');
+let ctx = canvas.getContext('2d');
 
 function drawFood(position) {
   ctx.fillStyle = config.foodColor;
@@ -86,7 +87,7 @@ function drawSnake(snake) {
 }
 
 function drawScore(score) {
-  var scoreValue = document.querySelector('.score-value');
+  let scoreValue = document.querySelector('.score-value');
   scoreValue.innerText = score;
 }
 
@@ -97,12 +98,12 @@ function drawBoard(fillColor, strokeColor) {
   ctx.fillRect(0, 0, canvas.height, canvas.width);
 
   ctx.beginPath();
-  for (var x = 0.5; x < canvas.width; x += config.cellSize.x) {
+  for (let x = 0.5; x < canvas.width; x += config.cellSize.x) {
     ctx.moveTo(x, 0);
     ctx.lineTo(x, canvas.height);
   }
 
-  for (var y = 0.5; y < canvas.height; y += config.cellSize.y) {
+  for (let y = 0.5; y < canvas.height; y += config.cellSize.y) {
     ctx.moveTo(0, y);
     ctx.lineTo(canvas.width, y);
   }
@@ -111,32 +112,12 @@ function drawBoard(fillColor, strokeColor) {
   ctx.closePath();
 }
 
-function drawTitle() {
-  ctx.fillStyle = '#fafafa';
-  ctx.textAlign = 'center';
-  ctx.font = '900 48px titillium web';
-  ctx.fillText('snake', canvas.width / 2, canvas.height / 3 - 24);
-}
-
-function drawSubTitle() {
-  ctx.fillStyle = '#fafafa';
-  ctx.textAlign = 'center';
-  ctx.font = '16px titillium web';
-  ctx.fillText('an adventure in learning reactive js', canvas.width / 2, canvas.height / 3);
-}
-
-function drawAuthor() {
-  ctx.fillStyle = '#fafafa';
-  ctx.textAlign = 'center';
-  ctx.font = '16px titillium web';
-  ctx.fillText('by fielding johnston', canvas.width / 2, canvas.height / 3 + 24);
-}
-
 function drawInstructions() {
   ctx.fillStyle = '#fafafa';
   ctx.textAlign = 'center';
-  ctx.font = '16px titillium web';
-  ctx.fillText('press any arrow key to start', canvas.width / 2, canvas.height / 1.5);
+  ctx.font = '20px Courier Prime';
+  ctx.fillText('press any arrow key to start', canvas.width / 2, canvas.height / 1.2);
+
 }
 
 // Game
@@ -146,55 +127,52 @@ function init() {
   canvas.style.outline = 'none';
   canvas.focus();
 
-  drawBoard(config.boardColor, config.boardLineColor);
-  drawTitle();
-  drawSubTitle();
-  drawAuthor();
+  drawBoard(config.boardColor);
   drawInstructions();
 }
 
 function update(state) {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  drawBoard(config.boardColor, config.boardLineColor);
+  drawBoard(config.boardColor);
   drawSnake(state.snake);
   drawFood(state.food);
   drawScore(state.score);
 }
 
-var move = R.curry(function (direction, snake) {
+let move = R.curry(function (direction, snake) {
   if (direction.additiveInverseOf(snake.dir)) {
     direction = snake.dir;
   }
   return { pos: snake.pos.add(direction.multiplyScalar(20)).mod(config.boardSize), dir: direction };
 });
 
-var keyMap = {
+let keyMap = {
   37: move(new Vector(-1, 0)),
   38: move(new Vector(0, -1)),
   39: move(new Vector(1, 0)),
   40: move(new Vector(0, 1))
+};
+// Config
 
-  // Config
-
-}; var config = {
-  tickerInterval: 50,
+let config = {
+  tickerInterval: 100,
   snakeSpeed: 1,
   cellSize: new Vector(20, 20),
   boardSize: new Vector(canvas.height, canvas.width),
-  boardColor: '#1b1b1b',
-  boardLineColor: '#222222',
-  snakeColor: '#ffffff',
+  boardColor: 'black',
+  snakeColor: 'blue',
   foodColor: '#f9165d'
 };
 
 config.initial = {
   food: Vector.random(config.boardSize, config.cellSize),
   snake: { pos: new Vector(0, 0), dir: new Vector(0, 1) }
+};
 
-  // Streams
+// Streams
 
-}; var ticker$ = Rx.Observable.interval(config.tickerInterval, Rx.Scheduler.requestAnimationFrame).map(function () {
+let ticker$ = Rx.Observable.interval(config.tickerInterval, Rx.Scheduler.requestAnimationFrame).map(function () {
   return { time: Date.now(), delta: null };
 }).scan(function (previous, current) {
   return {
@@ -203,43 +181,43 @@ config.initial = {
   };
 });
 
-var input$ = Rx.Observable.fromEvent(document, 'keydown').flatMap(function (event) {
+let input$ = Rx.Observable.fromEvent(document, 'keydown').flatMap(function (event) {
   return R.contains(event.keyCode, Object.keys(keyMap).map(function (n) {
     return parseInt(n);
   })) ? [keyMap[event.keyCode]] : [];
 });
 
-var snakeHead$ = ticker$.withLatestFrom(input$).scan(function (snake, ref) {
-  var ref2 = slicedToArray(ref, 2),
+let snakeHead$ = ticker$.withLatestFrom(input$).scan(function (snake, ref) {
+  let ref2 = slicedToArray(ref, 2),
     ticker = ref2[0],
     dir = ref2[1];
 
   return dir(snake);
 }, config.initial.snake);
 
-var food$ = snakeHead$.distinctUntilChanged().scan(function (food, snakeHead) {
+let food$ = snakeHead$.distinctUntilChanged().scan(function (food, snakeHead) {
   return food.equals(snakeHead.pos) ? Vector.random(config.boardSize, config.cellSize) : food;
 }, config.initial.food).distinctUntilChanged().share();
 
-var length$ = food$.scan(function (prevLength, apple) {
+let length$ = food$.scan(function (prevLength, apple) {
   return prevLength + 1;
 }, 1);
 
-var score$ = length$.map(function (n) {
+let score$ = length$.map(function (n) {
   return (n - 2) * 10;
 });
 
-var snake$ = snakeHead$.withLatestFrom(length$).scan(function (prevSnake, ref3) {
-  var ref4 = slicedToArray(ref3, 2),
+let snake$ = snakeHead$.withLatestFrom(length$).scan(function (prevSnake, ref3) {
+  let ref4 = slicedToArray(ref3, 2),
     head = ref4[0],
     length = ref4[1];
 
-  var snakeHistory = R.union(prevSnake, [head]);
-  var snake = R.takeLast(length - 1, snakeHistory);
+  let snakeHistory = R.union(prevSnake, [head]);
+  let snake = R.takeLast(length - 1, snakeHistory);
   return snake;
 }, []);
 
-var game$ = Rx.Observable.combineLatest(food$, score$, snake$, ticker$, function (food, score, snake, ticker) {
+let game$ = Rx.Observable.combineLatest(food$, score$, snake$, ticker$, function (food, score, snake, ticker) {
   return {
     food: food,
     score: score,
@@ -248,7 +226,7 @@ var game$ = Rx.Observable.combineLatest(food$, score$, snake$, ticker$, function
   };
 });
 
-// Do it to it
+// Do it
 
 init();
 game$.subscribe(function (state) {
